c327e18fb99f8bb4e5ba1b8709503ea8949c57c8
Kolyankovszky <kolyankovszkyg@kkszki.hu>
2023-02-03 21:26:56 +0100

    Kezdo fajl BackEnd

---
+++ VizsgaBackEnd/feladatAlap/feladatAlap/Models/Employe.cs
@@ -0,0 +1,11 @@
+﻿namespace feladatAlap.Models
+{
+    public class Employe
+    {
+        public long id { get; set; }
+        public string name { get; set; }
+        public int age { get; set; }
+        public string description { get; set; }
+
+    }
+}
